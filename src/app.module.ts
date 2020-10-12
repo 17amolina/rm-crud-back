b9eea84a7fe3d467012103ed7bbf34189d3e9f95
17amolina <antonio.progmac@gmail.com>
2020-10-12 18:46:56 -0500

    connect db test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb+srv://testuser:4APWK5m^CuT8@guiltyspark.qsbai.mongodb.net/RoomieTest?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/product/dto/product.dto.ts
@@ -0,0 +1,9 @@
+export class CreateProductDTO {
+    //idProduct: GUID;
+    NameProduct: string;
+    Category: string;
+    Description: string;
+    ProductQuantity: Number;
+    status: boolean;
+    //TimeStamp: timestamp;
+}
